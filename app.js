c5b922e0198705bf23c4849d8bfdf401117970dc
oluwaseun <asunioluwaseun@gmail.com>
2020-02-27 14:46:55 +0000

    Created Routes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
 
+
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-	res.send("this will be the landing page soon!");
+	res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+	var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+	];
+	
+	res.render("campgrounds");
+	
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
