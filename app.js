9b29ac020502926925992860b0d29f70a570ceb0
oluwaseun <asunioluwaseun@gmail.com>
2020-03-10 16:33:14 +0000

    Added info to campground

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUn
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// var campgrounds = [
+// var Campgrounds= [
 //         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
 //         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
 //         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
@@ -36,7 +36,8 @@ app.get("/campgrounds", function(req, res){
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 //compiles schema into a model
@@ -45,7 +46,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 // 	{
 // 		name: "Salmon Creek",
-// 		image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+// 		image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",
+// 		description: "I really do not know what to put in this field but it is surely a campground"
 // 	}, function(err, campground){
 // 		if(err){
 // 			consoleole.log(err);
@@ -128,6 +130,18 @@ app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
 });
 
+//SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	//find the campground with Provided ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			//render show template with the found campground
+			res.render("show", {campground: foundCampground})
+		}
+	});
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("The YelpCamp Server has statred");
