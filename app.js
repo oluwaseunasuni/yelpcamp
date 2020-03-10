03e380a3104b2530e9f16a4ce1ddab2ff7f7f6d7
oluwaseun <asunioluwaseun@gmail.com>
2020-03-10 17:26:19 +0000

    fixed bug that hinders descripting from being saved to be db

--- app.js
+++ app.js
@@ -113,8 +113,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 app.post("/campgrounds", function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
-	var description = req.body.description;
-	var newCampground = {name: name, image: image};
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
 	// Create a new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
 		if(err){
